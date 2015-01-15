b6d73967c411fae22315287fd49b20d16037e001
Adeel Younas <P13230057@GH582-014.lec-admin.dmu.ac.uk>
2015-01-15 16:19:29 +0000

    adding.c is a program which allows two numbers to be added

---
+++ adding.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{  
+          int a, b, c;
+          printf("Enter two numbers\n");
+          scanf("%d%d", &a, &b);
+
+          c = a + b;
+          
+          printf("sum of numbers = %d\n", c);
+
+          return 0;
+}
